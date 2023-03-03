375e87de4f3c1de565a2538c6e6ae629052e45b6
iBlurBlur <tanakorn.ngam@gmail.com>
2023-03-03 09:57:59 +0700

    Revise DTOs

--- Application/Features/Products/Models/EditProductDTO.cs
+++ Application/Features/Products/Models/EditProductDTO.cs
@@ -1,11 +1,7 @@
-﻿using Application.Features.ProductCategories.Models;
-using System.ComponentModel.DataAnnotations;
-
-namespace Application.Features.Products.Models;
+﻿namespace Application.Features.Products.Models;
 
 public class EditProductDTO : CreateProductDTO
 {
-    [Required]
     public int ProductId { get; set; }
 
     public Guid Rowguid { get; set; }
